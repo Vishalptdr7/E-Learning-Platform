14cfdb3023c07ca5d47d9b3a6bf9c619c2782006
Vishal Patidar <patidarvishal233@gmail.com>
2025-09-02 19:33:12 +0530

    commited

--- backend/db.js
+++ backend/db.js
@@ -1,24 +1,33 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST, // uniquevishal.mysql.database.azure.com
+  user: process.env.DB_USER, // vishal@uniquevishal
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    ca: fs.readFileSync("./DigiCertGlobalRootG2.crt.pem"), // or Baltimore root cert
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 });
-
 
 const promisePool = pool.promise();
 
 const testConnection = async () => {
   try {
-    await promisePool.query('SELECT 1');
-    console.log('Database connection successful.');
+    await promisePool.query("SELECT 1");
+    console.log("✅ Database connection successful.");
   } catch (err) {
-    console.error('Database connection failed:', err.message);
+    console.error("❌ Database connection failed:", err.message);
   }
 };
 
